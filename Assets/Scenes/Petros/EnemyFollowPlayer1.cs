fbd0904c00c7dc31e014652f2427dca714c356ab
Petros <petrospapa21@gmail.com>
2018-07-29 20:36:17 +0300

    created game master script

---
+++ Assets/Scenes/Petros/EnemyFollowPlayer1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPlayer1 : MonoBehaviour {
+
+    private float followingEnemySpeed = 10;
+
+    public GameObject Player;
+    public GameObject FollowingEnemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
