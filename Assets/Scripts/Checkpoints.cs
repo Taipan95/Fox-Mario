c8ada1b4d1163ea66c59feb7cb05d2ca25859e22
Unknown <nasos.karachalios.stagas@gmail.com>
2018-08-01 00:02:55 +0300

    lose And CheckpointScript uPDATED cOMMENTS AND FIXED WILL BE ADDED LATER

--- Assets/Scripts/Checkpoints.cs
+++ Assets/Scripts/Checkpoints.cs
@@ -1,48 +1,53 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Checkpoints : MonoBehaviour {
-//    // a list with The Checkpoints
-//    public List<GameObject> checkpointArray = new List<GameObject>();
-//    //gameobjects that will have to be teleported on loose
-//    public GameObject player, cameraMain;
-//    //creating a private collider for personal convenience
-//    private BoxCollider2D CheckCollider;
-//    //it starts from zero the FIRST CHECKPOINT IN THE GAME must be an invisible spawn point
-//    private int checkpointNumber=0;
-//    //a public float that it will be usefull to tune the camera at our needs
-//    public float cameraDistance;
-//    // Use this for initialization
-//    void Start () {
-		
-//	}
+public class Checkpoints : MonoBehaviour {
+    // a list with The Checkpoints
+    public List<GameObject> checkpointArray = new List<GameObject>();
+    //gameobjects that will have to be teleported on loose
+    public GameObject player, cameraMain;
+    //creating a private collider for personal convenience
+    private BoxCollider2D CheckCollider;
+    //it starts from zero the FIRST CHECKPOINT IN THE GAME must be an invisible spawn point
+    private int checkpointNumber=0;
+    //a public float that it will be usefull to tune the camera at our needs
+    public float cameraDistanceZ,cameraSettingWhenTeleports;
+    private float currentPlayerPositionx;
+    public bool respawnIsSet;
+    private float ycameraAxes;
+    // Use this for initialization
+    void Start () {
+        ycameraAxes = cameraMain.transform.position.y;
+        Debug.Log(+ycameraAxes+"edo einai ^_^");
+
+    }
 	
-//	// Update is called once per frame
-//	void Update () {
-		
-//	}
-//    void OnTriggerEnter2D(Collider2D CheckpointMark)
-//    {   //if the object collided is a checkpoint then checkpoint number is increased by one the colllider of that object turns of
-//        if (CheckpointMark.tag.ToLower().Contains("checkpoint"))
-//        {
-//            checkpointNumber++;
-//            CheckCollider = checkpointArray[checkpointNumber].GetComponent<BoxCollider2D>();
-//            CheckCollider.enabled = false;
-//        }
-//    }
-//    //this is a public method that is called by the general script LoseScript
-//    public void OnLoose(GameObject Player)
-//    {
-//<<<<<<< Updated upstream
-//        cameraMain.transform.Translate(0,0,0);
-//        cameraMain.transform.position = new Vector3(checkpointArray[checkpointNumber].transform.position.x+12, checkpointArray[checkpointNumber].transform.position.y,GetComponent<Camera>().transform.position.z);
-//=======
-//        cameraMain.transform.Translate(0, 0,0);
-//        cameraMain.transform.position = new Vector3(checkpointArray[checkpointNumber].transform.position.x+12, checkpointArray[checkpointNumber].transform.position.y, cameraDistance);
-//>>>>>>> Stashed changes
-//        player.transform.position = checkpointArray[checkpointNumber].transform.position;
-        
-//    }
+	// Update is called once per frame
+	void Update () {
+ 
+	}
+    void OnTriggerEnter2D(Collider2D coli)
+    {   //if the object collided is a checkpoint then checkpoint number is increased by one the colllider of that object turns of
+        if (coli.tag.ToLower().Contains("checkpoint"))
+        {
+            checkpointNumber++;
+            CheckCollider = checkpointArray[checkpointNumber].GetComponent<BoxCollider2D>();
+            CheckCollider.enabled = false;
+        }
 
-//}
+    }
+   
+      
+   
+    //this is a public method that is called by the general script LoseScript
+    public void OnLoose(GameObject Player)
+    {
+        cameraMain.transform.Translate(0, 0,0);
+        cameraMain.transform.position = new Vector3(checkpointArray[checkpointNumber].transform.position.x+cameraSettingWhenTeleports, ycameraAxes, cameraDistanceZ);
+        player.transform.position = checkpointArray[checkpointNumber].transform.position;
+   
+
+    }
+
+}
--- Assets/Scripts/LoseScript.cs
+++ Assets/Scripts/LoseScript.cs
@@ -1,27 +1,88 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-////stil under development but needed in order for the checkpoints to work
-//public class LoseScript : MonoBehaviour {
 
-//	// Use this for initialization
-//	void Start () {
-		
-//	}
-	
-//	// Update is called once per frame
-//	void Update () {
-		
-//	}
-//    void OnTriggerEnter2D(Collider2D Coli)
-//    {
-//        if (Coli.tag.ToLower().Contains("enemy"))
-//        {
-//            gameObject.GetComponent<Checkpoints>().OnLoose(this.gameObject);
-//        }
-//        if (Coli.name.ToLower().Contains("death"))
-//        {
-//            gameObject.GetComponent<Checkpoints>().OnLoose(this.gameObject);
-//        }
-//    }
-//}
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//stil under development but needed in order for the checkpoints to work
+public class LoseScript : MonoBehaviour {
+    public double timer;
+    public double deathDelay;
+    public bool isHit = false,free;
+    private float currentPlayerPositionx;
+	// Use this for initialization
+	void Start () {
+}
+	// Update is called once per frame
+	void Update () {
+        if (isHit)
+        {
+            timer+=Time.deltaTime;
+            //Debug.Log(+timer);
+            if (timer > deathDelay)
+            {  if (isHit = true)
+                {
+                    free = true;
+                }
+                if (free)
+                {
+                        isHit = false;
+                        
+                    afterDeath();
+
+
+
+
+
+                }
+            }
+            }
+        if (timer >= deathDelay) { timer = 0; }
+     
+        
+
+        
+    }
+    void OnTriggerEnter2D(Collider2D Coli)
+    {
+        currentPlayerPositionx = gameObject.transform.position.x;
+        if (Coli.tag.ToLower().Contains("enemy"))
+        {
+         isHit = true;
+            onDeath();   
+
+        }
+        if (Coli.name.ToLower().Contains("death"))
+        {
+        currentPlayerPositionx = gameObject.transform.position.x;
+
+            isHit = true;
+            onDeath();
+
+        }
+    }
+    private void afterDeath()
+    { 
+        //gameObject.transform.position = gameObject.transform.position;
+     
+            Debug.Log("entered");
+
+        PlayerControl.Instance.animator.SetBool("Dead", false);
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.GetComponent<PlayerControl>().enabled = true;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+            gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        free = true;
+        gameObject.GetComponent<Checkpoints>().OnLoose(this.gameObject);
+
+    }
+    private void onDeath()
+    {
+        PlayerControl.Instance.animator.SetBool("Dead", true);
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        gameObject.GetComponent<PlayerControl>().enabled = false;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        free = false;
+       
+    }
+
+}
