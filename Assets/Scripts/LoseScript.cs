0b68d2331af65ac2952d85507d36ec5b033eb2a1
Petros <petrospapa21@gmail.com>
2018-08-03 01:36:02 +0300

    .

--- Assets/Scripts/LoseScript.cs
+++ Assets/Scripts/LoseScript.cs
@@ -1,55 +1,57 @@
 
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 //stil under development but needed in order for the checkpoints to work
-public class LoseScript : MonoBehaviour {
+public class LoseScript : MonoBehaviour
+{
     public double timer;
     //the time which the death will last
     public double deathDelay;
     //ishit is used in case the object is been hit and free is used a a boolean that checks if the death delay is over
-    public bool isHit = false,free;
-	// Use this for initialization
-	void Start () {
-}
-	// Update is called once per frame
-	void Update () {
+    public bool isHit = false, free;
+
+
+    // Update is called once per frame
+    void Update()
+    {
 
         //it checks if the target is hit if it is a timer is started
         if (isHit)
         {
-            timer+=Time.deltaTime;
+            timer += Time.deltaTime;
             //the time which it take before the respawn
             if (timer > deathDelay)
-            {              
-                    free = true;
+            {
+                free = true;
 
                 //if free is true then isHitsIs turned of since the object is no longer under the grasp of thanatos also ressurection starts to take place
                 if (free)
                 {
-                    isHit = false;    
+                    isHit = false;
                     afterDeath();
                 }
             }
         }
         //when the timer gets greater than the time needed for the death to take place it Resets to 0
         if (timer >= deathDelay) { timer = 0; }
-          
+
     }
 
 
     void OnTriggerEnter2D(Collider2D Coli)
-    { 
+    {
         //on trigger the is hit is set to true also the effects of thanatos start to take place also it checks if the gameObject sould die or not by the Collision with the enemy
-        if (Coli.tag.ToLower().Contains("enemy") &&gameObject.transform.position.y<=Coli.transform.position.y)
+        if (Coli.tag.ToLower().Contains("enemy") && gameObject.transform.position.y <= Coli.transform.position.y)
         {
-         isHit = true;
-            onDeath();   
+            isHit = true;
+            onDeath();
         }
         //the same as above only this time is on the invisible deathzone this might change a tiny bit in the future if it starts causing problems and might only the ressurection part takes place
         if (Coli.name.ToLower().Contains("death"))
         {
-        //currentPlayerPositionx = gameObject.transform.position.x;
+            //currentPlayerPositionx = gameObject.transform.position.x;
 
             isHit = true;
             onDeath();
@@ -66,16 +68,16 @@ public class LoseScript : MonoBehaviour {
     //this method is used after death is over it turns off the animation and also reenables the collider the movement of the playerr the velocity and is set back to dynamic
     //also some variables are been reset and the onLooseMethod is called from checkpoints so the player will be teleported there
     private void afterDeath()
-    { 
+    {
         //gameObject.transform.position = gameObject.transform.position;
-     
-            Debug.Log("entered");
+
+        Debug.Log("entered");
 
         PlayerControl.Instance.animator.SetBool("Dead", false);
-            gameObject.GetComponent<BoxCollider2D>().enabled = true;
-            gameObject.GetComponent<PlayerControl>().enabled = true;
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, gameObject.GetComponent<Rigidbody2D>().velocity.y);
-            gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        gameObject.GetComponent<PlayerControl>().enabled = true;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
         free = true;
         gameObject.GetComponent<Checkpoints>().OnLoose(this.gameObject);
 
@@ -92,7 +94,7 @@ public class LoseScript : MonoBehaviour {
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
         free = false;
-       
+
     }
 
 }
