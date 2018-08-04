0346b98adcb6a321abbd2e100337e51372cd8b3f
Unknown <nasos.karachalios.stagas@gmail.com>
2018-08-04 13:52:32 +0300

    SimpleAiEnemyScriptFinished

---
+++ Assets/Scenes/Nahx/SimpleEnemyAi.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemyAi : MonoBehaviour {
+    public float velocityOfGAMEobject;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(velocityOfGAMEobject, 0);
+
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        velocityOfGAMEobject = -velocityOfGAMEobject;
+    } private void OnTriggerEnter2D(Collider2D col)
+    {
+        velocityOfGAMEobject = -velocityOfGAMEobject;
+    }
+}
