4fc46e814cf7bd6b82a1ef510b6175f67cea3226
Unknown <nasos.karachalios.stagas@gmail.com>
2018-08-10 16:48:29 +0300

    Uploaded the scene so far in order to get contructive critisizm and remove what you dont like
    plus extra sprites

--- Assets/Scripts/NewCameraMovementScript.cs
+++ Assets/Scripts/NewCameraMovementScript.cs
@@ -23,7 +23,7 @@ public class NewCameraMovementScript : MonoBehaviour {
             gameObject.GetComponent<Rigidbody2D>().velocity =new Vector2(0f, gameObject.GetComponent<Rigidbody2D>().velocity.y);
         }
         //the same as is was before
-        if (Mathf.Abs(gameObject.transform.position.y + 7 - player.transform.position.y) >= 10)
+        if (Mathf.Abs(gameObject.transform.position.y + 7 - player.transform.position.y) >= 15)
         {  //an h thesi tou y toy pexti einai megaliteri apo ayth ths kameras tote dn metakinite
             if (player.transform.position.y > gameObject.transform.position.y)
             {
